ea582e08a3cbb686c2c84aa590350de927b4a7be
Gustavob23 <bueninn15@gmail.com>
2023-08-24 13:06:37 -0300

    .

--- script.js
+++ script.js
@@ -1,26 +1,17 @@
 document.getElementById("calculate").addEventListener("click", function() {
-    const principal = parseFloat(document.getElementById("principal").value);
-    const rate = parseFloat(document.getElementById("rate").value);
-    const startDateString = document.getElementById("startDate").value;
-    const endDateString = document.getElementById("endDate").value;
-  
-    const startDate = new Date(startDateString);
-    const endDate = new Date(endDateString);
-  
-    const diffTime = Math.abs(endDate - startDate);
-    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)) + 1; // Adding 1 to include both start and end days
-  
-    const monthlyInterest = (principal * rate * diffDays) / 30 / 100; // Assuming 30 days in a month
-    const dailyInterest = monthlyInterest / 30; // Assuming 30 days in a month
-  
-    document.getElementById("result").textContent = `
-      Juros Mensais: R$ ${monthlyInterest.toFixed(2)}
-      Juros Diários: R$ ${dailyInterest.toFixed(2)}
-    `;
-  });
-  
-  
-  
-  
-  
-  
+  const principal = parseFloat(document.getElementById("principal").value);
+  const rate = parseFloat(document.getElementById("rate").value);
+  const startDate = new Date(document.getElementById("startDate").value);
+  const endDate = new Date(document.getElementById("endDate").value);
+
+  const timeDiff = Math.abs(endDate.getTime() - startDate.getTime());
+  const diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); // Difference in days
+
+  const totalInterest = (principal * rate * diffDays) / 30 / 100; // Assuming 30 days in a month
+  const totalAmount = principal + totalInterest;
+
+  document.getElementById("result").textContent = `
+    Juros Totais: R$ ${totalInterest.toFixed(2)}
+    Valor Total: R$ ${totalAmount.toFixed(2)}
+  `;
+});
