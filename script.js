20e83e8af08ae432dd929d3b0643c4820453f0a3
Gustavob23 <bueninn15@gmail.com>
2023-08-24 13:39:41 -0300

    .

--- script.js
+++ script.js
@@ -8,15 +8,21 @@ document.getElementById("calculate").addEventListener("click", function() {
   let diffDays = 0;
 
   while (currentDate <= endDate) {
-    diffDays++;
-    currentDate.setDate(currentDate.getDate() + 1);
     if (currentDate.getDate() === 31) {
-      currentDate.setMonth(currentDate.getMonth() + 1);
-      currentDate.setDate(1);
+      diffDays++;
+    } else if (currentDate.getDate() === 30) {
+      // Handle 30-day months
+      if (currentDate.getMonth() === 3 || currentDate.getMonth() === 5 || currentDate.getMonth() === 8 || currentDate.getMonth() === 10) {
+        diffDays++;
+      }
+    } else {
+      diffDays++;
     }
+
+    currentDate.setDate(currentDate.getDate() + 1);
   }
 
-  const totalInterest = (principal * rate * diffDays) / (30 * 100);
+  const totalInterest = (principal * rate * diffDays) / 30 / 100;
   const totalAmount = principal + totalInterest;
 
   // Exibir os resultados
@@ -25,4 +31,3 @@ document.getElementById("calculate").addEventListener("click", function() {
     Valor Total: R$ ${totalAmount.toFixed(2)}
   `;
 });
-
