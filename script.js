24a3169a9e034614812886fd00fdf830f4d038b8
Gustavob23 <bueninn15@gmail.com>
2023-08-24 13:34:10 -0300

    .

--- script.js
+++ script.js
@@ -4,14 +4,28 @@ document.getElementById("calculate").addEventListener("click", function() {
   const startDate = new Date(document.getElementById("startDate").value);
   const endDate = new Date(document.getElementById("endDate").value);
 
-  const timeDiff = Math.abs(endDate.getTime() - startDate.getTime());
-  const diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); // Difference in days
+  let diffDays = 0;
 
-  const totalInterest = (principal * rate * diffDays) / (30 * 100); // Assuming 30 days in a month
+  const currentDate = new Date(startDate);
+  while (currentDate <= endDate) {
+    diffDays++;
+    currentDate.setMonth(currentDate.getMonth() + 1);
+    currentDate.setDate(1);
+    currentDate.setDate(31);
+  }
+
+  const totalInterest = (principal * rate * diffDays) / (30 * 100);
   const totalAmount = principal + totalInterest;
 
+  // Exibir os resultados
   document.getElementById("result").innerHTML = `
     Juros Totais: R$ ${totalInterest.toFixed(2)}<br>
     Valor Total: R$ ${totalAmount.toFixed(2)}
   `;
+
+  // Limpar os campos de entrada
+  document.getElementById("principal").value = "";
+  document.getElementById("rate").value = "";
+  document.getElementById("startDate").value = "";
+  document.getElementById("endDate").value = "";
 });
