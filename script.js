7d988990192abaff81df79cc23b3c4e8cef2e8d5
Gustavob23 <bueninn15@gmail.com>
2023-08-24 13:42:37 -0300

    .

--- script.js
+++ script.js
@@ -10,6 +10,25 @@ document.getElementById("calculate").addEventListener("click", function() {
   while (currentDate <= endDate) {
     diffDays++;
     currentDate.setDate(currentDate.getDate() + 1);
+    
+    if (currentDate.getDate() === 31 && currentDate.getMonth() === 0) {
+      currentDate.setMonth(currentDate.getMonth() + 1);
+    }
+    if (currentDate.getDate() === 31 && currentDate.getMonth() === 2) {
+      currentDate.setMonth(currentDate.getMonth() + 1);
+    }
+    if (currentDate.getDate() === 31 && currentDate.getMonth() === 4) {
+      currentDate.setMonth(currentDate.getMonth() + 1);
+    }
+    if (currentDate.getDate() === 31 && currentDate.getMonth() === 6) {
+      currentDate.setMonth(currentDate.getMonth() + 1);
+    }
+    if (currentDate.getDate() === 31 && currentDate.getMonth() === 7) {
+      currentDate.setMonth(currentDate.getMonth() + 1);
+    }
+    if (currentDate.getDate() === 31 && currentDate.getMonth() === 9) {
+      currentDate.setMonth(currentDate.getMonth() + 1);
+    }
   }
 
   const totalInterest = (principal * rate * diffDays) / (30 * 100);
