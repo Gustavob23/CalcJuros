8541badd259f83f07e583af4485cb588e7afa7c1
Gustavob23 <bueninn15@gmail.com>
2023-08-24 14:34:10 -0300

    .

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById("calculate").addEventListener("click", function() {
   const principal = parseFloat(document.getElementById("principal").value);
-  const rate = parseFloat(document.getElementById("rate").value);
+  const rate = parseFloat(document.getElementById("rate").value) / 100;
   const startDate = new Date(document.getElementById("startDate").value);
   const endDate = new Date(document.getElementById("endDate").value);
 
@@ -14,27 +14,21 @@ document.getElementById("calculate").addEventListener("click", function() {
     diffDays++;
 
     if (currentDate.getDate() === 31) {
-      const monthlyInterest = (principal * rate * daysInBaseMonth) / (daysInYear * 100);
-      const dailyInterest = monthlyInterest / daysInBaseMonth;
-      const extraDayInterest = dailyInterest * (31 - daysInBaseMonth);
-      diffDays += extraDayInterest;
+      diffDays += daysInBaseMonth - 1;
     } else if (currentDate.getDate() === 28 || currentDate.getDate() === 29) {
-      const monthlyInterest = (principal * rate * daysInBaseMonth) / (daysInYear * 100);
-      const dailyInterest = monthlyInterest / daysInBaseMonth;
-      const missingDaysInterest = dailyInterest * (daysInBaseMonth - currentDate.getDate());
-      diffDays -= missingDaysInterest;
+      diffDays -= daysInBaseMonth - currentDate.getDate();
     }
 
     currentDate.setDate(currentDate.getDate() + 1);
   }
 
-  const totalInterest = (principal * rate * diffDays) / (daysInYear * 100);
+  const totalInterest = (principal * rate * diffDays) / daysInYear;
   const totalAmount = principal + totalInterest;
 
   // Exibir os resultados
   document.getElementById("result").innerHTML = `
     Juros Totais: R$ ${totalInterest.toFixed(2)}<br>
     Valor Total: R$ ${totalAmount.toFixed(2)}<br>
-    Quantidade de Dias: ${diffDays.toFixed(2)}
+    Quantidade de Dias: ${diffDays}
   `;
 });
