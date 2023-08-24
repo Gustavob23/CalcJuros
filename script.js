cec898bd9881337a07bd3644894e81169202e410
Gustavob23 <bueninn15@gmail.com>
2023-08-24 13:09:52 -0300

    .

--- script.js
+++ script.js
@@ -7,11 +7,11 @@ document.getElementById("calculate").addEventListener("click", function() {
   const timeDiff = Math.abs(endDate.getTime() - startDate.getTime());
   const diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); // Difference in days
 
-  const totalInterest = (principal * rate * diffDays) / 30 / 100; // Assuming 30 days in a month
+  const totalInterest = (principal * rate * diffDays) / (30 * 100); // Assuming 30 days in a month
   const totalAmount = principal + totalInterest;
 
-  document.getElementById("result").textContent = `
-    Juros Totais: R$ ${totalInterest.toFixed(2)}
+  document.getElementById("result").innerHTML = `
+    Juros Totais: R$ ${totalInterest.toFixed(2)}<br>
     Valor Total: R$ ${totalAmount.toFixed(2)}
   `;
 });
